b716499847deb2e8f8b8c98f1e45c53a64fded58
Thomas <tg.59290@gmail.com>
2019-05-06 11:46:23 +0200

    add comments in js and html

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var user = {
   age : 43,
   paiement: ["visa", "mastercard", "american express", "maestro"],
 
-  // Methodes
+  // Methodes to display a specific property in the corresponding list item in HTML
   getFirstName : function() {
     var container = document.getElementById('firstName');
     container.innerHTML += this.firstName;
@@ -27,14 +27,17 @@ var user = {
   },
   getPaiement : function() {
     var container = document.getElementById('paiement')
+    // Loop through the paiements array to add each type of card
     for (var i = 0; i < this.paiement.length; i++) {
       card = this.paiement[i];
       container.innerHTML += card.charAt(0).toUpperCase() + card.slice(1);
+      // Check if it is the last iteration to add a comma or not
       if(i < this.paiement.length -1 ) {
         container.innerHTML += ", ";
       }
     }
   },
+  // Final method to show all the properties at once
   showInfo : function() {
     this.getFirstName();
     this.getLastName();
@@ -44,17 +47,20 @@ var user = {
   }
 };
 
-
+// Object to deal with the validation of a user
 var userChecker = {
-  // Properties
+  // Properties with the reference values
   allowedLocations : ["FRANCE", "GERMANY", "SWEDEN", "SPAIN"],
   allowedPaiement : ["VISA", "MAESTRO"],
   minimumAge: 16,
+  // Standard alerts from Bootstrap 4
   error: "<div class='alert alert-danger' role='alert'></div>",
   success: "<div class='alert alert-success' role='alert'></div>",
+  // Retrieve the message container to add the alerts
   messageContainer: document.getElementById('message'),
 
-  // Methodes
+  // Methodes to check the properties fo a user
+  //Check if the user location is one of the allowed locations
   checkLocation : function(user) {
     var location = user.location.toUpperCase();
     if (this.allowedLocations.includes(location)) {
@@ -66,7 +72,7 @@ var userChecker = {
       this.messageContainer.lastChild.innerHTML = "Pays d'origine non autorisé";
     }
   },
-
+  // Check if the user is older than the minimum age
   checkAge : function(user) {
     if (user.age >= this.minimumAge) {
       this.messageContainer.innerHTML += this.success;
@@ -80,13 +86,16 @@ var userChecker = {
 
   checkPaiment : function(user) {
     var message = "Vous pouvez payer avec : ";
+    // reference variable to check if at least one user card is allowed
     var detection = false;
+    // Check if each user card is allowed
     for(var i = 0; i< user.paiement.length; i++) {
       if(this.allowedPaiement.includes(user.paiement[i].toUpperCase())) {
         detection = true;
         message += user.paiement[i] + " ";
       }
     }
+    // If at least one card is allowed
     if(detection) {
       this.messageContainer.innerHTML += this.success;
       this.messageContainer.lastChild.innerHTML = message;
@@ -96,7 +105,7 @@ var userChecker = {
       this.messageContainer.lastChild.innerHTML = "Aucun de vos moyens de paiment ne peut être utilisé";
     }
   },
-
+  // final function to check all the user properties at once
   checkUser : function(user) {
     this.checkLocation(user);
     this.checkAge(user);
@@ -104,4 +113,5 @@ var userChecker = {
   }
 };
 
+// By default the user information are displayed on the page
 user.showInfo();
